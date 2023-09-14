46f19078560d28a83a4f7f80c97748dac26f28a6
maandrea-cm <maandrea@uninorte.edu.co>
2023-09-13 23:06:37 -0500

    start icon

--- pagina-web/src/componentes/mapa/react-leaflet-icon.js
+++ pagina-web/src/componentes/mapa/react-leaflet-icon.js
@@ -2,6 +2,7 @@ import L from 'leaflet';
 import icon from 'leaflet/dist/images/marker-icon.png';
 import iconShadow from 'leaflet/dist/images/marker-shadow.png';
 import redcircle from '/redcircle.png'
+import start from '/start.jpg'
 
 export const MarkerIcon = L.icon({
     iconUrl: icon,
@@ -14,3 +15,8 @@ export const CircleIcon = L.icon({
     iconUrl: redcircle,
     iconSize:[10,10],
 });
+
+export const StartIcon = L.icon({
+    iconUrl : start,
+    iconSize: [30,30]
+})
